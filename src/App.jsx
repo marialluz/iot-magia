4e64770500da6e6960ef4d9ab41572914a26713a
marialluz <maria.luz.117@ufrn.edu.br>
2022-12-12 15:29:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
+
+
+function App() {
+  return (
+    <ThemeProvider>
+      
+    </ThemeProvider>
+  );
+}
+
+export default App;
